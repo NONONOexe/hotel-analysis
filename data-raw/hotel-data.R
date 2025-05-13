6ec1f3f38f89fd3225260f166d705777f06c4cdf
Keisuke ANDO <ando@maslab.aitech.ac.jp>
2025-05-13 10:57:13 +0900

    :sparkles: Add example code to visualize hotel coords

--- data-raw/hotel-data.R
+++ data-raw/hotel-data.R
@@ -45,3 +45,35 @@ routes <- sf::st_sf(routes_df)
 readr::write_rds(kanazawa_hotels, here::here("myapp", "kanazawa_hotels.rds"))
 readr::write_rds(kanazawa_poi, here::here("myapp", "kanazawa_poi.rds"))
 readr::write_rds(routes, here::here("myapp", "routes.rds"))
+
+# 可視化の例
+library(sf)
+src <- kanazawa_hotels[1, ]
+dst <- kanazawa_poi[1, ]
+route <- osrm::osrmRoute(src, dst, osrm.profile = "foot")
+
+kanazawa_hotels |>
+  dplyr::slice_min(order_by = weekend, n = 5) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_bar(
+    ggplot2::aes(
+      x = reorder(stringr::str_sub(name, end = 5), weekend),
+      y = weekend
+    ),
+    stat = "identity",
+    width = 0.6
+  ) +
+  ggplot2::theme_minimal(base_size = 24, base_family = "IPAexゴシック") +
+  ggplot2::theme(axis.title = ggplot2::element_blank())
+
+ggplot2::ggplot() +
+  ggplot2::geom_sf(data = src, size = 5) +
+  ggplot2::geom_sf(data = dst, size = 5) +
+  ggplot2::geom_sf(data = route) +
+  ggplot2::theme_minimal(base_size = 24, base_family = "IPAexゴシック") +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 60))
+
+mapgl::maplibre(bounds = route) |>
+  mapgl::add_circle_layer(id = "hotel-layer", source = src) |>
+  mapgl::add_circle_layer(id = "poi-layer", source = dst) |>
+  mapgl::add_line_layer(id = "route-layer", source = route)
